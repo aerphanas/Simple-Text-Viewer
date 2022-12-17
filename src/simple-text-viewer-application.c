56e6a0b44d68f57485843eb14156acf4bee9f701
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2022-12-17 12:38:15 +0700

    Loading Content From A File
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- src/simple-text-viewer-application.c
+++ src/simple-text-viewer-application.c
@@ -121,5 +121,8 @@ simple_text_viewer_application_init (SimpleTextViewerApplication *self)
   gtk_application_set_accels_for_action (GTK_APPLICATION (self),
                                          "app.quit",
                                          (const char *[]) { "<primary>q", NULL });
+    gtk_application_set_accels_for_action (GTK_APPLICATION (self),
+                                         "win.open",
+                                         (const char *[]) { "<Ctrl>o", NULL });
 }
 
--- src/simple-text-viewer-window.c
+++ src/simple-text-viewer-window.c
@@ -35,6 +35,7 @@ struct _SimpleTextViewerWindow
   GtkHeaderBar        *header_bar;
   GtkLabel            *label;
   GtkTextView         *main_text_view;
+  GtkButton           *open_button;
 };
 
 G_DEFINE_FINAL_TYPE (SimpleTextViewerWindow, simple_text_viewer_window, ADW_TYPE_APPLICATION_WINDOW)
@@ -48,10 +49,139 @@ simple_text_viewer_window_class_init (SimpleTextViewerWindowClass *klass)
   gtk_widget_class_bind_template_child (widget_class, SimpleTextViewerWindow, header_bar);
   gtk_widget_class_bind_template_child (widget_class, SimpleTextViewerWindow, label);
   gtk_widget_class_bind_template_child (widget_class, SimpleTextViewerWindow, main_text_view);
+  gtk_widget_class_bind_template_child (widget_class, SimpleTextViewerWindow, open_button);
+}
+
+static void
+open_file_complete (GObject          *source_object,
+                    GAsyncResult     *result,
+                    SimpleTextViewerWindow *self)
+{
+  GFile *file = G_FILE (source_object);
+
+  g_autofree char *contents = NULL;
+  gsize length = 0;
+
+  g_autoptr (GError) error = NULL;
+
+  // Complete the asynchronous operation; this function will either
+  // give you the contents of the file as a byte array, or will
+  // set the error argument
+  g_file_load_contents_finish (file,
+                               result,
+                               &contents,
+                               &length,
+                               NULL,
+                               &error);
+
+  // Query the display name for the file
+  g_autofree char *display_name = NULL;
+  g_autoptr (GFileInfo) info =
+    g_file_query_info (file,
+                       "standard::display-name",
+                       G_FILE_QUERY_INFO_NONE,
+                       NULL,
+                       NULL);
+  if (info != NULL)
+    {
+      display_name =
+        g_strdup (g_file_info_get_attribute_string (info, "standard::display-name"));
+    }
+  else
+    {
+      display_name = g_file_get_basename (file);
+    }
+
+  // In case of error, print a warning to the standard error output
+  if (error != NULL)
+    {
+      g_printerr ("Unable to open “%s”: %s\n",
+                  g_file_peek_path (file),
+                  error->message);
+      return;
+    }
+  // Ensure that the file is encoded with UTF-8
+  if (!g_utf8_validate (contents, length, NULL))
+    {
+      g_printerr ("Unable to load the contents of “%s”: "
+                  "the file is not encoded with UTF-8\n",
+                  g_file_peek_path (file));
+      return;
+    }
+
+  // Retrieve the GtkTextBuffer instance that stores the
+  // text displayed by the GtkTextView widget
+  GtkTextBuffer *buffer = gtk_text_view_get_buffer (self->main_text_view);
+
+  // Set the text using the contents of the file
+  gtk_text_buffer_set_text (buffer, contents, length);
+
+  // Reposition the cursor so it's at the start of the text
+  GtkTextIter start;
+  gtk_text_buffer_get_start_iter (buffer, &start);
+  gtk_text_buffer_place_cursor (buffer, &start);
+  // Set the title using the display name
+  gtk_window_set_title (GTK_WINDOW (self), display_name);
+}
+
+static void
+open_file (SimpleTextViewerWindow *self,GFile *file)
+{
+  g_file_load_contents_async (file,
+                              NULL,
+                              (GAsyncReadyCallback) open_file_complete,
+                              self);
+}
+
+static void
+on_open_response (GtkNativeDialog  *native,
+                  int               response,
+                  SimpleTextViewerWindow *self)
+{
+  // If the user selected a file...
+  if (response == GTK_RESPONSE_ACCEPT)
+    {
+      GtkFileChooser *chooser = GTK_FILE_CHOOSER (native);
+
+      // ... retrieve the location from the dialog...
+      g_autoptr (GFile) file = gtk_file_chooser_get_file (chooser);
+
+      // ... and open it
+      open_file (self, file);
+    }
+
+  // Release the reference on the file selection dialog now that we
+  // do not need it any more
+  g_object_unref (native);
+}
+
+static void
+text_viewer_window__open_file_dialog (GAction          *action G_GNUC_UNUSED,
+                                      GVariant         *parameter G_GNUC_UNUSED,
+                                      SimpleTextViewerWindow *self)
+{
+  // Create a new file selection dialog, using the "open" mode
+  GtkFileChooserNative *native =
+    gtk_file_chooser_native_new ("Open File",
+                                 GTK_WINDOW (self),
+                                 GTK_FILE_CHOOSER_ACTION_OPEN,
+                                 "_Open",
+                                 "_Cancel");
+
+  // Connect the "response" signal of the file selection dialog;
+  // this signal is emitted when the user selects a file, or when
+  // they cancel the operation
+  g_signal_connect (native, "response", G_CALLBACK (on_open_response), self);
+
+  // Present the dialog to the user
+  gtk_native_dialog_show (GTK_NATIVE_DIALOG (native));
 }
 
 static void
 simple_text_viewer_window_init (SimpleTextViewerWindow *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
+  g_autoptr (GSimpleAction) open_action = g_simple_action_new ("open", NULL);
+  g_signal_connect (open_action, "activate", G_CALLBACK (text_viewer_window__open_file_dialog), self);
+  g_action_map_add_action (G_ACTION_MAP (self), G_ACTION (open_action));
 }
