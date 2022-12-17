ac007300ab51cdb7a0c6cf0f18c6dd0cfd4f8a71
Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>
2022-12-17 13:32:27 +0700

    Showing The Cursor Position
    Signed-off-by: Muhammad Aviv Burhanudin <muhamadaviv14@gmail.com>

--- src/simple-text-viewer-window.c
+++ src/simple-text-viewer-window.c
@@ -36,6 +36,7 @@ struct _SimpleTextViewerWindow
   GtkLabel            *label;
   GtkTextView         *main_text_view;
   GtkButton           *open_button;
+  GtkLabel            *cursor_pos;
 };
 
 G_DEFINE_FINAL_TYPE (SimpleTextViewerWindow, simple_text_viewer_window, ADW_TYPE_APPLICATION_WINDOW)
@@ -50,6 +51,7 @@ simple_text_viewer_window_class_init (SimpleTextViewerWindowClass *klass)
   gtk_widget_class_bind_template_child (widget_class, SimpleTextViewerWindow, label);
   gtk_widget_class_bind_template_child (widget_class, SimpleTextViewerWindow, main_text_view);
   gtk_widget_class_bind_template_child (widget_class, SimpleTextViewerWindow, open_button);
+  gtk_widget_class_bind_template_child (widget_class, SimpleTextViewerWindow, cursor_pos);
 }
 
 static void
@@ -178,10 +180,39 @@ text_viewer_window__open_file_dialog (GAction          *action G_GNUC_UNUSED,
 }
 
 static void
+text_viewer_window__update_cursor_position (GtkTextBuffer    *buffer,
+                                            GParamSpec       *pspec G_GNUC_UNUSED,
+                                            SimpleTextViewerWindow *self)
+{
+  int cursor_pos = 0;
+
+  // Retrieve the value of the "cursor-position" property
+  g_object_get (buffer, "cursor-position", &cursor_pos, NULL);
+
+  // Construct the text iterator for the position of the cursor
+  GtkTextIter iter;
+  gtk_text_buffer_get_iter_at_offset (buffer, &iter, cursor_pos);
+
+  // Set the new contents of the label
+  g_autofree char *cursor_str =
+    g_strdup_printf ("Ln %d, Col %d",
+                     gtk_text_iter_get_line (&iter) + 1,
+                     gtk_text_iter_get_line_offset (&iter) + 1);
+
+  gtk_label_set_text (self->cursor_pos, cursor_str);
+}
+
+static void
 simple_text_viewer_window_init (SimpleTextViewerWindow *self)
 {
   gtk_widget_init_template (GTK_WIDGET (self));
   g_autoptr (GSimpleAction) open_action = g_simple_action_new ("open", NULL);
   g_signal_connect (open_action, "activate", G_CALLBACK (text_viewer_window__open_file_dialog), self);
   g_action_map_add_action (G_ACTION_MAP (self), G_ACTION (open_action));
+
+  GtkTextBuffer *buffer = gtk_text_view_get_buffer (self->main_text_view);
+  g_signal_connect (buffer,
+                    "notify::cursor-position",
+                    G_CALLBACK (text_viewer_window__update_cursor_position),
+                    self);
 }
